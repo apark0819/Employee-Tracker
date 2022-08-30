d0362d866d4646ee3627b6536657ed6cad4ecdf7
apark0819 <apark0819@gmail.com>
2022-08-29 22:48:57 -0700

    connection to db

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+// Dependencies
 const inquirer = require('inquirer')
 const mysql = require('mysql2')
 const cTable = require('console.table')
 require('dotenv').config() 
+
+// Connection to DB
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: process.env.user,
+        password: process.env.pass,
+        database: process.env.database
+    },
+    console.log(`Connected to ${process.env.database} database`)
+)
