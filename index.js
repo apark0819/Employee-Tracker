47e8a47b675a4ca6c51db9ebe41c71f2ca3be25d
apark0819 <apark0819@gmail.com>
2022-08-29 22:52:01 -0700

    prompt async function

--- index.js
+++ index.js
@@ -23,11 +23,44 @@ const promptAction = () => {
             type: 'list',
             name: 'action',
             message: "What would you like to do?",
-            choices:["view all departments","view all roles","view all employees","add a department","add a role","add an employee","update an employee","exit"]
+            choices:["View all departments","View all roles","View all employees","Add a department","Add a role","Add an employee","Update an employee","Exit"]
         }
         ]).then(response => {
             const {action} = response
             routeAction(action)
         })
+}
+
+// Await First Action Command
+const routeAction = async (action) => {
+    let exit = false
+    switch(action){
+        case "exit":
+            exit = true
+            break
+        case "View all departments":
+            await viewDepartments()
+            break
+        case "View all roles":
+            await viewRoles()
+            break
+        case "View all employees":
+            await viewEmployees()
+            break
+        case "Add a department":
+            await addDepartment()
+            break
+        case "Add a role":
+            await addRole()
+            break
+        case "Add an employee":
+            await addEmployee()
+            break
+        case "Update an employee":
+            await updateEmployee()
+            break
+    }
+    if(!exit) promptAction()
+    else process.exit()
 }
 
