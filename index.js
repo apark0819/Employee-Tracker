3d7a11b811d0dfbb8a62699178351ac6aa8766f8
apark0819 <apark0819@gmail.com>
2022-08-29 22:49:46 -0700

    first inquirer prompt

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to ${process.env.database} database`)
 )
+
+// First Action
+const promptAction = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: "What would you like to do?",
+            choices:["view all departments","view all roles","view all employees","add a department","add a role","add an employee","update an employee","exit"]
+        }
+        ]).then(response => {
+            const {action} = response
+            routeAction(action)
+        })
+}
+
